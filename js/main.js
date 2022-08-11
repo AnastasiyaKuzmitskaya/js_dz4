5c67956de264b028c5a01319a468ce2213ab2f72
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-08-11 17:06:27 +0300

    task_1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 "use strict";
 
+//task 1
+const myCity={
+  city: 'Grodno',
+  country: 'Belarus'
+
+}
+console.log(myCity);
+      delete myCity.country;   
+      console.log(myCity);
+
+
+const yearOffoundation='year';
+myCity[yearOffoundation]='1128';
+console.log(myCity);
+                             /////////в чем разница
+
+myCity.country= 'Belarus';
+console.log(myCity);
+
